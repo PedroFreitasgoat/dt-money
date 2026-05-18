5c11da66dc3dda30f98e9415dcc573c9115d2fab
pedro <pedrogithb1@gmail.com>
2026-05-18 15:37:53 -0300

    Tabela de transações

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,11 +1,40 @@
 import { Header } from "../../components/Header";
 import { Sumarry } from "../../components/Header/Sumarry";
+import {
+  PriceHighLight,
+  TransactionsContainer,
+  TransactionsTable,
+} from "./styles";
 
 export function Transactions() {
-    return (
-        <div>
-            <Header />
-            <Sumarry />
-        </div>
-    )
+  return (
+    <div>
+      <Header />
+      <Sumarry />
+
+      <TransactionsContainer>
+        <TransactionsTable>
+          <tbody>
+            <tr>
+              <td width="50%">Desenvolvimento de site</td>
+              <td>
+                <PriceHighLight variant="income">R$ 12.000,00</PriceHighLight>
+              </td>
+              <td>Venda</td>
+              <td>18/05/2026</td>
+            </tr>
+            <tr>
+              <td width="50%">Hamburger</td>
+
+              <td>
+                <PriceHighLight variant="outcome">- R$ 59,00</PriceHighLight>
+              </td>
+              <td>Alimentação</td>
+              <td>18/05/2026</td>
+            </tr>
+          </tbody>
+        </TransactionsTable>
+      </TransactionsContainer>
+    </div>
+  );
 }
